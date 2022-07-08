1b089290dcd17a01e75a705edcc71490480730ac
JiaY-shi <shi05275@163.com>
2022-07-08 13:54:40 +0800

    Update glinet-ax1800.yml

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ else
 fi
 
 if [ ! -n "$profile" ]; then
-	profile=glinet-ax1800
+	profile=target_wlan_ap-gl-ax1800
 fi
 
 echo "Start..."
@@ -28,10 +28,17 @@ cp -r custom/  $base/gl-infra-builder/feeds/custom/
 cp -r *.yml $base/gl-infra-builder/profiles
 cd $base/gl-infra-builder
 #setup
+
+if [[ $profile == *5-4* ]]; then
+        python3 setup.py -c config-wlan-ap-5.4.yml
+else
+        python3 setup.py -c config-wlan-ap.yml
+fi
+
 python3 setup.py -c config-wlan-ap-5.4.yml
 
 cd wlan-ap/openwrt
-./scripts/gen_config.py $profile glinet_depends
+./scripts/gen_config.py $profile glinet_depends custom
 
 git clone https://github.com/gl-inet/glinet4.x.git $base/glinet
 ./scripts/feeds update -a 
