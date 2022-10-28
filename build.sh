cc4b37922b34dbb725d9ff92137a92afde66684f
JiaY-shi <shi05275@163.com>
2022-10-28 10:32:27 +0800

    add gl-mt2500 support

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-
 CRTDIR=$(pwd)
-
 base=$1
 profile=$2
 ui=$3
@@ -24,8 +21,6 @@ fi
 if [ ! -n "$ui" ]; then
         ui=true
 fi
-
-
 echo "Start..."
 
 #clone source tree 
@@ -33,42 +28,54 @@ git clone https://github.com/gl-inet/gl-infra-builder.git $base/gl-infra-builder
 cp -r custom/  $base/gl-infra-builder/feeds/custom/
 cp -r *.yml $base/gl-infra-builder/profiles
 cd $base/gl-infra-builder
-#setup
-
-if [[ $profile == *5-4* ]]; then
-        python3 setup.py -c configs/config-wlan-ap-5.4.yml
-elif [[ $profile == *a1300* ]]; then
-		python3 setup.py -c configs/config-21.02.2.yml
-else
-        python3 setup.py -c configs/config-wlan-ap.yml
-fi
-
-if [[ $profile == *wlan_ap*  ]]; then
-	ln -s $base/gl-infra-builder/wlan-ap/openwrt ~/openwrt
-else
-	ln -s $base/gl-infra-builder/openwrt-21.02/openwrt-21.02.2 ~/openwrt
-fi
-cd ~/openwrt
 
 
-if [[ $ui == true  ]] && [[ $profile == *wlan_ap* ]]; then 
-	./scripts/gen_config.py $profile glinet_depends custom
-else
-	./scripts/gen_config.py $profile openwrt_common luci custom
-fi
+function build_firmware(){
+    cd ~/openwrt
+    need_gl_ui=$1
+    # fix helloword build error
+    rm -rf feeds/packages/lang/golang
+    svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang feeds/packages/lang/golang
+    #install feed 
+    ./scripts/feeds update -a && ./scripts/feeds install -a && make defconfig
+    #build 
+    if [[ $need_gl_ui == true  ]]; then 
+        make -j$(expr $(nproc) + 1) GL_PKGDIR=~/glinet/ipq60xx/ V=s
+    else
+        make -j$(expr $(nproc) + 1)  V=s
+    fi
+}
 
-# fix helloword build error
-rm -rf feeds/packages/lang/golang
-svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang feeds/packages/lang/golang
-
-git clone https://github.com/gl-inet/glinet4.x.git $base/glinet
-./scripts/feeds update -a 
-./scripts/feeds install -a
-make defconfig
-
-if [[ $ui == true  ]] && [[ $profile == *wlan_ap* ]]; then 
-	make -j$(expr $(nproc) + 1) GL_PKGDIR=$base/glinet/ipq60xx/ V=s
-else
-	make -j$(expr $(nproc) + 1)  V=s
-fi
+case $profile in 
+    target_wlan_ap-gl-ax1800|\
+    target_wlan_ap-gl-axt1800|\
+    target_wlan_ap-gl-ax1800-5-4|\
+    target_wlan_ap-gl-axt1800-5-4)
+        if [[ $profile == *5-4* ]]; then
+            python3 setup.py -c configs/config-wlan-ap-5.4.yml
+        else
+            python3 setup.py -c configs/config-wlan-ap.yml
+        fi
+        ln -s $base/gl-infra-builder/wlan-ap/openwrt ~/openwrt && cd ~/openwrt
+        if [[ $ui == true  ]]; then 
+	        ./scripts/gen_config.py $profile glinet_depends custom
+            git clone https://github.com/gl-inet/glinet4.x.git ~/glinet
+        else
+	        ./scripts/gen_config.py $profile openwrt_common luci custom
+        fi
+        build_firmware $ui
+    ;;
+    target_ipq40xx_gl-a1300)
+        python3 setup.py -c configs/config-21.02.2.yml
+        ln -s $base/gl-infra-builder/openwrt-21.02/openwrt-21.02.2 ~/openwrt && cd ~/openwrt
+        ./scripts/gen_config.py $profile openwrt_common luci custom
+        build_firmware 
+    ;;
+	target_mt7981_gl-mt2500)
+		python3 setup.py -c configs/config-mt7981-21.02.3.yml
+		ln -s $base/gl-infra-builder/mt7981 ~/openwrt && cd ~/openwrt	
+		./scripts/gen_config.py $profile  custom
+		build_firmware
+	;;
+esac
 
